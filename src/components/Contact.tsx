b8a61bef691e300c7f26032912581d18ea4541cd
Gabriel Colares <nxsskill@gmail.com>
2025-04-29 20:38:21 -0300

    fix: remover importação não utilizada do componente Contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "../ui/button"
 import { Input } from "../ui/input"
 import { Textarea } from "../ui/textarea"
-import { EnvelopeSimple, Phone, TiktokLogo, LinkedinLogo, InstagramLogo } from "@phosphor-icons/react"
+import { EnvelopeSimple, Phone, LinkedinLogo, InstagramLogo } from "@phosphor-icons/react"
 
 const formSchema = z.object({
   name: z.string().min(1, "Nome é obrigatório"),
